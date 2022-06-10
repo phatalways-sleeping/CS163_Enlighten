712653013a00e701991e7d457a0f7591fbe1cb94
Phát Trần <trankim147@gmail.com>
2022-06-10 17:53:18 +0700

    Update login

--- CS163_Enlighten/Constants.h
+++ CS163_Enlighten/Constants.h
@@ -1,1 +1,7 @@
 #pragma once
+#include<string>
+using std::string;
+const string ADMIN = "Data//admins.csv";
+const string USERS = "Data//users.csv";
+const int SEARCH_HISTORY = 1;
+const int FAVORITE_LIST = 2;
