73c00bf6cdb22fcd2429adcd11fdd85dae6495a3
Vinh Hoang Nhu <68021529+vinhhn2210@users.noreply.github.com>
2022-06-29 11:26:34 +0700

    Update test_function.h

--- CS163_Enlighten/test_function.h
+++ CS163_Enlighten/test_function.h
@@ -1,5 +1,5 @@
 #pragma once
-const int special_value = 2;
+const int special_value = 1;
 vector <pair<string, bool>> nullvector();
 int likeStatus(string user, vector <pair<string, bool>> list, int& cur_id);
 bool like_compare(UserDef x, UserDef y);
